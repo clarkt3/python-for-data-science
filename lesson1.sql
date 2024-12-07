8eb2aae1b71caf99d75e86decbae589c06b8183e
Tom Clark <tclark21@me.com>
2024-12-06 22:45:59 -0600

    add WHERE statments to note

--- lesson1.sql
+++ lesson1.sql
@@ -60,3 +60,22 @@ LIMIT 20;
 -- WHERE goes after SELECT & FROM but before ORDER BY & LIMIT
 
 -- WHERE is similar to FILTER in excel or google sheets
+
+/*  Query that pulls 5 rows and all columns from the orders table
+    that have a dollar amount of gloss_amt_usd greater than or equal to 1000
+*/
+
+SELECT *
+FROM orders
+WHERE gloss_amt_usd >= 1000
+LIMIT 5;
+
+/*
+    QUERY: pulls the fist 10 rows and all columns from the orders table that
+    have a total_amt_usd less than 500
+*/
+
+SELECT *
+FROM orders
+WHERE gloss_amt_usd < 500
+LIMIT 10;
