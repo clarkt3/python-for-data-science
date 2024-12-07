156cb7acdc9a1cc39f8294158778a4b00a8e5150
Tom Clark <tclark21@me.com>
2024-12-06 22:34:30 -0600

    add WHERE clause note

--- lesson1.sql
+++ lesson1.sql
@@ -26,3 +26,37 @@ SELECT *
 FROM orders
 ORDER BY occurred_at DESC
 LIMIT 1000;
+
+-- write a query that returns 10 earliest (DESC) order in the orders table
+-- include the id, occurred_at, total_amt_usd
+
+SELECT id, occurred_at, total_amt_usd
+FROM orders
+ORDER BY occurred_at 
+LIMIT 10;
+
+-- New query return the top 5 orders in terms of the largest total_amt_usd
+-- include the id, account_id, and total_amt_usd
+
+SELECT id, account_id, total_amt_usd
+FROM orders
+ORDER BY total_amt_usd DESC
+LIMIT 5;
+
+-- New Query to return the lowest 20 orders in terms of the smallest total_amt_usd
+-- include the id, account_id, total_amt_usd
+
+SELECT id, accout_id, total_amt_usd
+FROM orders
+ORDER BY total_amt_usd
+LIMIT 20;
+
+-- Evaluator was used to check all these answers before submittal
+
+-- don't forget your semicolons at the end of each statement
+
+-- WHERE clause allows you to filter a set of results basedon specific criteria
+
+-- WHERE goes after SELECT & FROM but before ORDER BY & LIMIT
+
+-- WHERE is similar to FILTER in excel or google sheets
